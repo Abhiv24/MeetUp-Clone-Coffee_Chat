9e617fb73a8b44b30a66778bfab02f51752950bb
Abhiv24 <99272624+Abhiv24@users.noreply.github.com>
2023-05-28 13:42:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+// var swiper=new Swiper('.carousel',{
+// 	navigation:{
+// 		nextE1: '.swiper-button-next',
+// 		precE1:'.swiper-button-prev',
+// 	},
+// 	pagination:{
+// 		el:',swiper-pagination',
+// 	},
+// })
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
